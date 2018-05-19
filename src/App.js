29d00c645107ef7a252f0b6d317dfb9d4a65ccb2
mike <dinhmq1@gmail.com>
2018-05-18 22:49:22 -0500

    sync react ui and state with controlled components

--- src/App.js
+++ src/App.js
@@ -3,26 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  // To give our component 'state', we'll start by adding a 'constructor' to add
-  // the component class add constructor keyword
   constructor() {
-    // We'll want to call 'super' to make sure that the constructor for the component 
-    // class that we're extending is called.
     super()
-    // now we can define the 'state' for this component
-    // and set that to equal an empty object. The state object
-    // will contain whatever data structure or components needs to
-    // render properly. In our case, this will include a list of todos
     this.state = {
-      // an array of 'todos' containing a list of objects,
-      // each object representing a todo.
       todos: [
         {id: 1, name: 'Learn JSX', isComplete: true},
         {id: 2, name: 'Build an Awesome App', isComplete: false},
         {id: 3, name: 'Ship it!', isComplete: false}
-      ]
-      // open chrome react-dev tool and view the array with state to verify
+      ],
+      currentTodo: ''
     }
+    // bind
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  // create an event handler that can capture the input and assign the value to the currentTodo state
+  handleInputChange(evt) {
+    this.setState({
+      currentTodo: evt.target.value
+    })
+    // Now we need to reference it in our current constructor and bind it to 'this'
   }
   render() {
     return (
@@ -33,13 +32,11 @@ class App extends Component {
         </header>
         <div className="Todo-App">
           <form>
-            <input type="text" />
+            {/* open react-dev tools and view the state currentTodo change */}
+            <input type="text" onChange={this.handleInputChange} value={this.state.currentTodo} />
           </form>
           <div>
             <ul>
-              {/* you will see this Warning: Each child in an array or iterator should have a unique "key" prop. 
-                to fix it, use the key property. In this case, we'll use 'id' as the key
-              */}
               {this.state.todos.map(todo => 
                 <li key={todo.id}>
                   <input type="checkbox" defaultChecked={todo.isComplete} /> {todo.name}
